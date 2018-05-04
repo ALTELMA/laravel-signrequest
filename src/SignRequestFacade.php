1a71664a812fe3c5b19f5473e84c2055c3472458
Phongthorn Khamkankaew <nrohtgnohp@gmail.com>
2018-05-04 12:13:09 +0700

    Create SignRequestFacade.php

---
+++ src/SignRequestFacade.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Altelma\LaravelSignRequest;
+
+use \Illuminate\Support\Facades\Facade;
+
+class SignRequestFacade extends Facade {
+
+    protected static function getFacadeAccessor() {
+        return 'SignRequest';
+    }
+}
