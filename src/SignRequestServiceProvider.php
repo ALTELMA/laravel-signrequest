90c24a1f138fd90be0f9cc4881bffe05a9a8fa6c
Phongthorn Khamkankaew <nrohtgnohp@gmail.com>
2018-05-08 02:28:11 +0000

    Apply fixes from StyleCI

--- src/SignRequestServiceProvider.php
+++ src/SignRequestServiceProvider.php
@@ -4,23 +4,22 @@ namespace Altelma\LaravelSignRequest;
 
 use Illuminate\Support\ServiceProvider;
 
-class SignRequestServiceProvider extends ServiceProvider {
-
+class SignRequestServiceProvider extends ServiceProvider
+{
     protected $defer = true;
 
     public function boot()
     {
-		$this->publishes([
-			__DIR__.'/../config/signrequest.php' => config_path('signrequest.php'),
-		], 'signrequest');
+        $this->publishes([
+            __DIR__.'/../config/signrequest.php' => config_path('signrequest.php'),
+        ], 'signrequest');
     }
-    
+
     public function register()
     {
-        $this->mergeConfigFrom( __DIR__.'/../config/signrequest.php', 'signrequest');
-        
-        $this->app->singleton('SignRequest', function($app)
-        {
+        $this->mergeConfigFrom(__DIR__.'/../config/signrequest.php', 'signrequest');
+
+        $this->app->singleton('SignRequest', function ($app) {
             $config = $app->make('config');
             $token = $config->get('signrequest.token');
             $subdomain = $config->get('signrequest.subdomain');
